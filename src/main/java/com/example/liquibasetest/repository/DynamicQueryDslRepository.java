9a260d6a975f26edd204e0feb7fc5eafdeb89a9c
Roma_Man <kjspo56@gmail.com>
2023-05-24 14:17:11 +0900

    reset

--- src/main/java/com/example/liquibasetest/repository/DynamicQueryDslRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.liquibasetest.repository;
-
-import com.example.liquibasetest.entity.DynamicEntity;
-
-public interface DynamicQueryDslRepository {
-    void insertDynamicEntity(String dynamicTableName, String dynamicColumnName, String dynamicColumnValue);
-}
